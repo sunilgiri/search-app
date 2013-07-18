0344b951473cdba53f1864854dad7e8c44e6a194
CyrilCherian <cyril@mobomo.com>
2013-07-18 16:48:02 +0530

    Test for getting the files in a folder.

--- src/test/java/org/qburst/search/MetaDataTest.java
+++ src/test/java/org/qburst/search/MetaDataTest.java
@@ -1,5 +1,8 @@
 package org.qburst.search;
 
+import java.io.File;
+import java.util.Date;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentInformation;
 import org.junit.Test;
@@ -9,6 +12,7 @@ import org.junit.Test;
 public class MetaDataTest {
 	@Test
 	public void test() throws Exception {
+		File file = new File("/home/user/my-stuffs/my-boox/Manning.MongoDB.in.Action.Dec.2011.pdf");
 		PDDocument doc = PDDocument.load("/home/user/my-stuffs/my-boox/Manning.MongoDB.in.Action.Dec.2011.pdf");
 		PDDocumentInformation info = doc.getDocumentInformation();
 		System.out.println( "Page Count=" + doc.getNumberOfPages() );
@@ -19,7 +23,8 @@ public class MetaDataTest {
 		System.out.println( "Creator=" + info.getCreator() );
 		System.out.println( "Producer=" + info.getProducer() );
 		System.out.println( "Creation Date=" + info.getCreationDate() );
-		System.out.println( "Modification Date=" + info.getModificationDate());
+		System.out.println( "Modification Date=" + new Date(file.lastModified()));
 		System.out.println( "Trapped=" + info.getTrapped() );
+		
 	}
 }
